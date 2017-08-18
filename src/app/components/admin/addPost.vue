0e6ca7a60d5e297f79569cad56c18f753ba43176
puronglong <puronglong@foxmail.com>
2017-08-18 17:34:49 +0800

    edit

--- src/app/components/admin/addPost.vue
+++ src/app/components/admin/addPost.vue
@@ -6,18 +6,22 @@
                     <el-input v-model="post.title"></el-input>
                 </el-form-item>
                 <el-form-item label="内容：">
-                    <el-input type="textarea" v-model="post.content"></el-input>
+                    <!-- <el-input type="textarea" v-model="post.content"></el-input>  -->
+                    <textarea id="editor" v-model="post.content"></textarea>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="submitPost('ruleForm')">提交</el-button>
                 </el-form-item>
-            </el-form>
+            </el-form> 
         </div>
     </div>
 </template>
 
 <script>
 import qs from 'qs';
+import SimpleMDE from 'simplemde';
+import '../../../assets/css/simplemde.css';
+import marked from 'marked';
 
 export default {
     name: 'addPost',
@@ -25,6 +29,30 @@ export default {
         return {
             post: {}
         };
+    },
+    mounted: function(){
+        var self = this;
+        var smde = new SimpleMDE({
+            element: document.getElementById('editor'),
+            autofocus: true,
+            autosave: true,
+            previewRender: function(plainText) {
+                return marked(plainText,{
+                    renderer: new marked.Renderer(),
+                    gfm: true,
+                    pedantic: false,
+                    sanitize: false,
+                    tables: true,
+                    breaks: true,
+                    smartLists: true,
+                    smartypants: true,
+                });
+            },
+        });
+        smde.codemirror.on("change", function(){
+            var value = smde.value();
+            self.post.content = value
+        });
     },
     methods: {
         submitPost: function () {
